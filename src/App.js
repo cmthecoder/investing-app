f91cc219cc9f26b8732390eddbd087a573835d10
Chris Mems <christopher.mems@.com>
2023-03-08 14:07:00 -0600

    set up components and pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+import Benefits from "./pages/Benefits";
+import History from "./pages/History";
+import Home from "./pages/Home";
+import Invest from "./pages/Invest";
+import Resources from "./pages/Resources";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <Routes>
+        <Route
+          path='/'
+          element={<Home />}
+        />
+        <Route
+          path='/history'
+          element={<History />}
+        />
+        <Route
+          path='/benefits'
+          element={<Benefits />}
+        />
+        <Route
+          path='/invest'
+          element={<Invest />}
+        />
+        <Route
+          path='/resources'
+          element={<Resources />}
+        />
+      </Routes>
+      <Footer />
     </div>
   );
 }
